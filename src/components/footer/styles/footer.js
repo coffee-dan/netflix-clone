719cacfdc5c10ccf8e59a98c5c27a776c901b2e6
coffee-dan <coffee-dan@protonmail.com>
2020-10-19 13:36:15 -0500

    Footer styling
    Some comments explaining media query responsive design techniques

--- src/components/footer/styles/footer.js
+++ src/components/footer/styles/footer.js
@@ -3,16 +3,55 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
     display: flex;
+    max-width: 1000px;
+    flex-direction: column;
+    padding: 70px 56px;
+    margin: auto;
+
+    @media (max-width: 1000px) {
+        padding: 70px;
+    }
 `;
 
-export const Column = styled.div``;
+export const Column = styled.div`
+    display: flex;
+    flex-direction: column;
+    text-align: left;
+`;
 
-export const Row = styled.div``;
+// clever response media queries here that adjust spacing of items within grid
+// so that the elements fit on screen next to each other for longer as the screen
+// shrinks
+export const Row = styled.div`
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(230px, 1fr));
+    grid-gap: 15px;
 
-export const Link = styled.a``;
+    @media (max-width: 1000px) {
+        grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
+    }
+`;
 
-export const Title = styled.p``;
+export const Link = styled.a`
+    color: #757575;
+    margin-bottom: 20px;
+    font-size: 13px;
+    text-direction: none;
+`;
 
-export const Text = styled.p``;
+export const Title = styled.p`
+    font-size: 16px;
+    color: #757575;
+    margin-bottom: 40px;
+`;
 
-export const Break = styled.div``;
+export const Text = styled.p`
+    font-size: 13px;
+    color: #757575;
+    margin-bottom: 40px;
+`;
+
+export const Break = styled.div`
+    flex-basis: 100%;
+    height: 0;
+`;
