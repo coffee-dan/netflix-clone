c5c86da1efe2c02ee7183454b4f969aed0eece9c
coffee-dan <coffee-dan@protonmail.com>
2020-10-21 21:56:05 -0500

    Header styling and temporary implementation on Home
    OptForm was added as main feature of Header as a temporary proof of concept
    After this a dedicated Feature component will be created

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,12 +3,20 @@ import { FaqsContainer } from '../containers/faqs'
 import { JumbotronContainer } from '../containers/jumbotron'
 import { FooterContainer } from '../containers/footer'
 import { HeaderContainer } from '../containers/header'
+import { OptForm } from '../components'
 
 export default function Home() {
     return (
         <>
+            {/* Need to figure out what goes in header, a main feature type thing */}
             <HeaderContainer>
-                <p>Hello</p>
+                <OptForm>
+                    <OptForm.Input placeholder="Email Address" />
+                    <OptForm.Button>Try it now</OptForm.Button>
+                    <OptForm.Break />
+                    <OptForm.Text>Ready to watch? Enter your email to create or restart
+                    your membership.</OptForm.Text>
+                </OptForm>
             </HeaderContainer>
             <JumbotronContainer />
             <FaqsContainer />
