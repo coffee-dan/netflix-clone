31b109ad7dd6fdd814634f1581c5eace5b69872f
coffee-dan <coffee-dan@protonmail.com>
2020-10-23 23:24:29 -0500

    Absolute baseline Firebase setup
    Currently not retrieving information from firestore

--- src/index.js
+++ src/index.js
@@ -2,14 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { GlobalStyles } from './global-styles'
 import { App } from './app'
+import { FirebaseContext } from './context/firebase'
+
+// some default info used here, will setup new firestore soon
+const config = {
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messageSenderId: '',
+    appId: '',
+}
+
+const firebase = window.firebase.initializeApp(config)
 
 // Using fragment to ensure only one parent element is sent to render()
 // while still including GlobalStyles
-
 ReactDOM.render(
     <>
-        <GlobalStyles />
-        <App />
+        <FirebaseContext.Provider value={{ firebase: window.firebase }}>
+            <GlobalStyles />
+            <App />
+        </FirebaseContext.Provider>
     </>, 
     document.getElementById('root')
 );
