46022fcde1a319c8c6c48e22936c5c53fdfa1010
coffee-dan <coffee-dan@protonmail.com>
2020-10-27 05:29:54 -0500

    Signin page complete and static text content update
    Footer links now filled
    Jumbotron text now matching American Netflix

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 import * as ROUTES from '../constants/routes'
 import { Form } from '../components'
 import { HeaderContainer } from '../containers/header'
+import { FooterContainer } from '../containers/footer'
 
 export default function Sigin() {
     // used to capture errors
@@ -22,36 +23,39 @@ export default function Sigin() {
     }
 
     return (
-        <HeaderContainer>
-            <Form>
-                <Form.Title>Signin</Form.Title>
-                {error && <Form.Error>{error}</Form.Error>}
+        <>
+            <HeaderContainer>
+                <Form>
+                    <Form.Title>Signin</Form.Title>
+                    {error && <Form.Error>{error}</Form.Error>}
 
-                <Form.Base onSubmit={handleSignin} method="POST">
-                    <Form.Input 
-                        placeholder="Email address"
-                        value={emailAddress}
-                        onChange={({ target }) => setEmailAddress(target.value)}
-                    />
-                    <Form.Input 
-                        type="password"
-                        value={password}
-                        autoComplete="off"
-                        placeholder="Password"
-                        onChange={({ target }) => setPassword(target.value)}
-                    />
-                    <Form.Submit disabled={ isInvalid } type="submit">
-                        Sign In
-                    </Form.Submit>
+                    <Form.Base onSubmit={handleSignin} method="POST">
+                        <Form.Input 
+                            placeholder="Email address"
+                            value={emailAddress}
+                            onChange={({ target }) => setEmailAddress(target.value)}
+                        />
+                        <Form.Input 
+                            type="password"
+                            value={password}
+                            autoComplete="off"
+                            placeholder="Password"
+                            onChange={({ target }) => setPassword(target.value)}
+                        />
+                        <Form.Submit disabled={ isInvalid } type="submit">
+                            Sign In
+                        </Form.Submit>
 
-                    <Form.Text>
-                        New to Netflix? <Form.Link to={ROUTES.SIGN_UP}>Sign up now.</Form.Link>
-                    </Form.Text>
-                    <Form.TextSmall>
-                        This page is protected by Google reCAPTCHA.
-                    </Form.TextSmall>
-                </Form.Base>
-            </Form>
-        </HeaderContainer>
+                        <Form.Text>
+                            New to Netflix? <Form.Link to={ROUTES.SIGN_UP}>Sign up now.</Form.Link>
+                        </Form.Text>
+                        <Form.TextSmall>
+                            This page is protected by Google reCAPTCHA.
+                        </Form.TextSmall>
+                    </Form.Base>
+                </Form>
+            </HeaderContainer>
+            <FooterContainer></FooterContainer>
+        </>
     )
 }
