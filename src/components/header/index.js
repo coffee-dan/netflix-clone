6075db15eb921b81135b0c4556711a57c7910947
coffee-dan <coffee-dan@protonmail.com>
2020-10-29 19:44:24 -0500

    Static elements for browse page
    Header elements and main feature image added
    Next up is search box and other expected browse page elements

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,7 +1,18 @@
 // Header Component
 import React from 'react'
 import { Link as ReactRouterLink } from 'react-router-dom'
-import { Background, Container, Logo, ButtonLink } from './styles/header';
+import { 
+    Group, 
+    Background, 
+    Link, 
+    Text, 
+    Container, 
+    Logo, 
+    ButtonLink,
+    Feature,
+    FeatureCallOut,
+    PlayButton
+} from './styles/header';
 
 export default function Header({ bg = true, children, ...restProps }) {
     return bg ? <Background {...restProps}>{children}</Background> : children
@@ -9,6 +20,10 @@ export default function Header({ bg = true, children, ...restProps }) {
 
 Header.Frame = function HeaderFrame({ children, ...restProps }) {
     return <Container {...restProps}>{children}</Container>
+}
+
+Header.Group = function HeaderGroup({ children, ...restProps }) {
+    return <Group {...restProps}>{children}</Group>
 }
 
 // logo is clickable element so it has href in "to" prop
@@ -20,6 +35,26 @@ Header.Logo = function HeaderLogo({ to, ...restProps }) {
     )
 }
 
+Header.Text = function HeaderText({ children, ...restProps }) {
+    return <Text {...restProps}>{children}</Text>
+}
+
+Header.Link = function HeaderLink({ children, ...restProps }) {
+    return <Link {...restProps}>{children}</Link>
+}
+
 Header.ButtonLink = function HeaderButtonLink({ children, ...restProps }) {
     return <ButtonLink {...restProps}>{children}</ButtonLink>
+}
+
+Header.Feature = function HeaderFeature({ children, ...restProps }) {
+    return <Feature {...restProps}>{children}</Feature>
+}
+
+Header.FeatureCallOut = function HeaderFeatureCallOut({ children, ...restProps }) {
+    return <FeatureCallOut {...restProps}>{children}</FeatureCallOut>
+}
+
+Header.PlayButton = function HeaderPlayButton({ children, ...restProps }) {
+    return <PlayButton {...restProps}>{children}</PlayButton>
 }
