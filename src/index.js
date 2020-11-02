d2a21c3af88a4998f2b559baee7b6e0e9ceebaf6
coffee-dan <coffee-dan@protonmail.com>
2020-11-02 14:07:10 -0600

    Firestore hydration instructions
    Commented out as they only need to be run once
    This logic for adding data needs to be improved as it seems insufficient for adding data as time goes on.

--- src/index.js
+++ src/index.js
@@ -5,11 +5,19 @@ import { App } from './app'
 import { FirebaseContext } from './context/firebase'
 import firebaseConfig from './config.json'
 
+// import { seedDatabase } from './seed'
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 // The app will not work without this information. If you are seeing this
 //  comment form GitHub you need to get your own firebase config details
 const firebase = window.firebase.initializeApp( firebaseConfig )
+
+//  This command along with the accompanying import statement are what hydrate
+//   the firestore using the logic provided by the course instructor. This only needs
+//   to be run once hence why it is commented out. Also it requires firestore rules
+//   that are permissive enough to let it run.
+// seedDatabase( firebase )
 
 // Using fragment to ensure only one parent element is sent to render()
 // while still including GlobalStyles
